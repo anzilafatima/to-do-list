b1b1b6f241be4d0cf82bf30d7dcdc86d455b2e3e
anzilafatima <165508713+anzilafatima@users.noreply.github.com>
2024-04-11 00:16:54 +0500

    created to-do-list

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what you want to add in your todos",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "do you want to add more?",
+            default: "false",
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
+;
